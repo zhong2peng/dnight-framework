032ab6abdaa9180fa3cb5347beb8dbaf14eee6b0
ZHONGPENG769 <zhongpeng769@pingan.com.cn>
2019-04-15 23:21:10 +0800

    Add 3 items

---
+++ dnight-leetcode/【015】【3Sum】/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Author: dnight
+ * Date: 2019/4/15 22:57
+ */
+public class Main {
+
+  public static void main(String[] args) {
+      Solution solution = new Solution();
+      System.out.println(solution.threeSum(new int[]{0,0,0,0}));
+      System.out.println(solution.threeSum(new int[]{1,-1,-1,0}));
+      System.out.println(solution.threeSum(new int[]{-2,0,1,1,2}));
+  }
+}
