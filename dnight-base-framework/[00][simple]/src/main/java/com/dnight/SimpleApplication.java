51a9661367d32d141410e5a351fee114d70186fd
ZHONGPENG769 <john@163.com>
2019-08-14 18:10:22 +0800

    commit

---
+++ dnight-base-framework/[00][simple]/src/main/java/com/dnight/SimpleApplication.java
@@ -0,0 +1,16 @@
+package com.dnight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZHONGPENG769
+ * @date 2019/8/14
+ */
+@SpringBootApplication
+public class SimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApplication.class, args);
+    }
+}
---
+++ dnight-base-framework/dnight-gecco/readme.md
@@ -0,0 +1,2 @@
+git项目：https://github.com/xtuhcy/gecco
+爬虫练习
