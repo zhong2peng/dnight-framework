51fc8cf615f8d94950191b04ae37aa354d91156a
ZHONGPENG769 <john@163.com>
2019-09-26 15:50:24 +0800

    commit

---
+++ dnight-leetcode/[0213][House Robber II]/src/Solution.java
@@ -0,0 +1,50 @@
+/**
+ * @author ZHONGPENG769
+ * @date 2019/9/26
+ */
+public class Solution {
+
+    /**
+     * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed.
+     * All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one.
+     * Meanwhile, adjacent houses have security system connected and it will automatically contact the police
+     * if two adjacent houses were broken into on the same night.
+     * <p>
+     * Given a list of non-negative integers representing the amount of money of each house, determine the maximum
+     * amount of money you can rob tonight without alerting the police.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: [2,3,2]
+     * Output: 3
+     * Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+     * because they are adjacent houses.
+     * Example 2:
+     * <p>
+     * Input: [1,2,3,1]
+     * Output: 4
+     * Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+     * Total amount you can rob = 1 + 3 = 4.
+     *
+     * @param nums
+     * @return
+     */
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
+    }
+
+    private int rob(int[] nums, int lo, int hi) {
+        int include = 0;
+        int exclude = 0;
+        for (int j = lo; j <= hi; j++) {
+            int i = include;
+            int e = exclude;
+            include = e + nums[j];
+            exclude = Math.max(e, i);
+        }
+        return Math.max(include, exclude);
+    }
+}
